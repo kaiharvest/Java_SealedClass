2b25ae0ae616ba1b32b68949368c18501b05d080
kaiharvest <indradwiprabowo27@gmail.com>
2026-02-21 20:40:35 +0700

    Sealed Class

---
+++ src/main/java/indradwiprabowo/sealedclass/data/Manager.java
@@ -0,0 +1,4 @@
+package indradwiprabowo.sealedclass.data;
+
+public final class Manager extends Employee{
+}
---
+++ src/test/java/indradwiprabowo/sealedclass/EmployeeTest.java
@@ -0,0 +1,18 @@
+package indradwiprabowo.sealedclass;
+
+import indradwiprabowo.sealedclass.data.Employee;
+import indradwiprabowo.sealedclass.data.Manager;
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+
+    @Test
+    void test() {
+        Employee employee = new Employee();
+    }
+
+    @Test
+    void manager() {
+        Manager manager = new Manager();
+    }
+}
